b75809c59f25813f92c5a74fda62c9526e60c2a8
dteran24 <dteran21@yahoo.com>
2023-10-09 20:54:46 -0500

    remove unused import

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
 import Navbar from './components/Navbar'
-import AboutMe from './pages/AboutMe'
+// import AboutMe from './pages/AboutMe'
 import Intro from './pages/Intro'
 import Projects from './pages/Projects'
 
