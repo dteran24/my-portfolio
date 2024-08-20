4447352d09034ca905eb8c9e938f6cad71757455
dteran24 <dteran21@yahoo.com>
2024-08-20 12:27:13 -0500

    css grid  and card hover fix

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -8,28 +8,43 @@ import { FaPaw, FaBookOpen } from "react-icons/fa";
 
 import Project from "../models/ProjectModel";
 import { useState } from "react";
+import { useMediaQuery } from "react-responsive";
 interface projectProp {
   project: Project;
 }
 function ProjectCard(props: React.PropsWithChildren<projectProp>) {
   const { project } = props;
   const [hover, setHover] = useState(false);
+  const [bttnContainerHover, setBttnContainerHover] = useState(false);
+
+  const isMobile = useMediaQuery({ query: "(max-width: 860px)" });
 
   const iconMapping: {
     [key: string]: JSX.Element;
   } = {
-    rocket: <BsRocketTakeoffFill color="black" fontSize="3rem" />,
-    store: <MdLocalGroceryStore color="lightblue" fontSize="3rem" />,
-    dumbell: <BiDumbbell color="gray" fontSize="3rem" />,
+    rocket: (
+      <BsRocketTakeoffFill
+        color="black"
+        fontSize={isMobile ? "8rem" : "3rem"}
+      />
+    ),
+    store: (
+      <MdLocalGroceryStore
+        color="lightblue"
+        fontSize={isMobile ? "8rem" : "3rem"}
+      />
+    ),
+    dumbell: <BiDumbbell color="gray" fontSize={isMobile ? "8rem" : "3rem"} />,
     paw: <FaPaw color="green" fontSize="8rem" />,
-    book: <FaBookOpen color="brown" fontSize="3rem" />,
+    book: <FaBookOpen color="brown" fontSize={isMobile ? "8rem" : "3rem"} />,
   };
+  console.log(hover);
   return (
     <>
       {project.id === 1 ? (
-        <div className={styles.card}>
+        <div className={`${styles.cardXL} ${styles.card}`}>
           <div className={styles.icon}>{iconMapping[project.icon]}</div>
-          <h3 className={styles["card-header"]}>{project.title}</h3>
+          <h3 className={styles["card-headerXL"]}>{project.title}</h3>
           <p>{project.description}</p>
           <div className={styles["bttn-container-first"]}>
             <a
@@ -64,8 +79,9 @@ function ProjectCard(props: React.PropsWithChildren<projectProp>) {
           <div
             className={styles["card-container"]}
             onMouseLeave={() => setHover(false)}
+            onMouseEnter={() => setHover(true)}
           >
-            <div className={styles.card} onMouseEnter={() => setHover(true)}>
+            <div className={styles.card}>
               <div className={styles["card-header"]}>
                 {iconMapping[project.icon]}
                 <h3>{project.title}</h3>
@@ -102,7 +118,7 @@ function ProjectCard(props: React.PropsWithChildren<projectProp>) {
             <div
               className={
                 hover
-                  ? styles["bttn-container"] + " " + styles.show
+                  ? `${styles["bttn-container"]} ${styles.show}`
                   : styles["bttn-container"]
               }
             >
